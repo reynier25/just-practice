a12e444881a4d1cd6aa83f4211fd3042de74f98c
reynier25 <leap@hudello.app>
2022-06-15 02:59:52 -0700

    sprinkle bugs everywhere

--- solve2.js
+++ solve2.js
@@ -1,5 +1,6 @@
-let ranges = [[0, 10, "red"], [2, 12, "blue"], [1, 6, "red"]];
+let someRanges = [[0, 10, "red"], [2, 12, "blue"], [1, 6, "red"]];
 // [[0, 2, "red"], [2, 12, "blue"]]
+// [[0, 6, "red"], [6, 12, "blue"]]
 /*
 
 */
@@ -19,10 +20,81 @@ function solve(ranges) {
         const [leftBoundStateIdx, rightBoundStateIdx] = [findBounds[0], findBounds[1]];
 
         //splice in RTA, resize other rectangles, merge where needed etc.
+        processBound(state, leftBoundStateIdx, leftBoundRTA, colorRTA, "left");
+        processBound(state, rightBoundStateIdx, rightBoundRTA, colorRTA, "right");
+        
+        let removeNRectangles = rightBoundStateIdx - leftBoundStateIdx;
+        console.log("remove n rectangles", removeNRectangles);
+        console.log("lbsidx", leftBoundStateIdx);
+        console.log("state before", state);
+        state.splice(leftBoundStateIdx, removeNRectangles, RTA);
+        console.log("state after", state);
 
+        //final step (?) check rectangles to immediate left and right for mergeability (same color)
+        let leftStateRectangle = null;
+        let rightStateRectangle = null;
+        let curIdx = leftBoundStateIdx + 1;
+        if (curIdx - 1 >= 0) {
+            leftStateRectangle = state[curIdx - 1];
+        }
+        if (curIdx + 1 < state.length) {
+            rightStateRectangle = state[curIdx + 1]
+        }
+        // console.log("should be null", leftStateRectangle);
+        if (leftStateRectangle) {
+            let leftStateRectangleColor = leftStateRectangle[2];
+            if (leftStateRectangleColor === colorRTA) {
+                let mergedRectangle = [leftStateRectangle[0], rightBoundRTA, colorRTA];
+                state.splice(curIdx, 2, mergedRectangle);
+            }
+        }
+        if (rightStateRectangle) {
+            let rightStateRectangleColor = rightStateRectangle[2];
+            if (rightStateRectangleColor === colorRTA) {
+                let mergedRectangle = [leftBoundRTA, rightStateRectangle[1], colorRTA];
+                state.splice(curIdx, 2, mergedRectangle);
+            }
+        }
+        console.log("state after loop", state);
     }
 
     return state;
+}
+
+function processBound(state, bound, boundRTA, colorRTA, bfoundRectangleSide) {
+    // console.log("the bound", bound);
+    let bfoundRectangle = state[bound];
+    let bfoundRectangleLeftBound = bfoundRectangle[0];
+    let bfoundRectangleRightBound = bfoundRectangle[1];
+    let bfoundRectangleColor = bfoundRectangle[2];
+
+    if (bfoundRectangleColor !== colorRTA) {
+        if (boundRTA === bfoundRectangleLeftBound || boundRTA === bfoundRectangleRightBound) {
+            return;
+        } else if (boundRTA > bfoundRectangleLeftBound && boundRTA < bfoundRectangleRightBound) {
+            if (bfoundRectangleSide === "left") {
+                bfoundRectangle[1] = boundRTA;
+            } else {
+                bfoundRectangle[0] = boundRTA;
+            }
+            return;
+        } else {
+            return;
+        }
+    } else {
+        if (boundRTA === bfoundRectangleLeftBound || boundRTA === bfoundRectangleRightBound) {
+            return;
+        } else if (boundRTA > bfoundRectangleLeftBound && boundRTA < bfoundRectangleRightBound) {
+            return;
+        } else {
+            if (bfoundRectangleSide === "left") {
+                bfoundRectangle[0] = boundRTA;
+            } else {
+                bfoundRectangle[1] = boundRTA;
+            }
+            return;
+        }
+    }
 }
 
 function bsearch(state, leftTarget, rightTarget) {
@@ -52,5 +124,11 @@ function bsearchTarget(state, target) {
             searchIdx = Math.floor((leftPointer+rightPointer) / 2);
         }
     }
+    if (searchIdx === -1) searchIdx = 0;
+    if (searchIdx === state.length) searchIdx = state.length - 1;
     return searchIdx;
 }
+
+
+
+console.log(solve(someRanges));
